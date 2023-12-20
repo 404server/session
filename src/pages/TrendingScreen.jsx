3e891f0bea57dbc8039ffe8d0e7692eb998601a4
sarvarsaidazov <mrsarvar418@gmail.com>
2023-12-21 02:12:20 +0600

    added some style files

--- src/pages/TrendingScreen.jsx
+++ src/pages/TrendingScreen.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from "../music";
+import { useNavigate } from "react-router-dom";
+import './styles/Trending.css';
+
+const PlaylistCard = ({ playlist, onPlay }) => (
+    <div className="playlist-card" onClick={() => onPlay(playlist.id)}>
+        <img src={playlist.images[0].url} alt={`${playlist.name} cover`} className="playlist-image"/>
+        <p className="playlist-name">{playlist.name}</p>
+    </div>
+);
 
 const TrendingScreen = () => {
+    const [release, setRelease] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const nav = useNavigate();
+
+    useEffect(() => {
+        api.get('/browse/featured-playlists').then(res => {
+            setRelease(res.data.playlists.items);
+            setLoading(false);
+        }).catch(err => {
+            console.error('Error fetching categories:', err);
+            setError(err);
+            setLoading(false);
+        });
+    }, []);
+
+    const playPlaylist = id => nav("/player", { state: { id } });
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error loading playlists</div>;
+
     return (
-        <div className="main-screen">
-            Trending
+        <div className="trending-screen">
+            {release.map(playlist => (
+                <PlaylistCard key={playlist.id} playlist={playlist} onPlay={playPlaylist} />
+            ))}
         </div>
     );
 };
